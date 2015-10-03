6a9a900b710fac1976d5ccbe4844a2026d1d83eb
Anatoly Pulyaevskiy <anatoly@sparkcentral.com>
2015-10-03 13:45:10 -0700

    Removed strict check of travis env value

--- test/setup.dart
+++ test/setup.dart
@@ -7,9 +7,7 @@ import 'dart:async';
 /// Docker Toolbox and OS X (sorry). The IP of `default` docker-machine will
 /// be used.
 Future<String> getDefaultHost() async {
-  Platform.environment.forEach((k, v) => print('$k : $v'));
-  if (Platform.environment.containsKey('TRAVIS') &&
-      Platform.environment['TRAVIS'] == true) {
+  if (Platform.environment.containsKey('TRAVIS')) {
     return '127.0.0.1';
   } else {
     var res = await Process.run('docker-machine', ['ip', 'default']);
