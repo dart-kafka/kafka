0f065a8bc925e5e2f04ac129cf6a794f6f931533
Anatoly Pulyaevskiy <anatoly@sparkcentral.com>
2016-06-27 19:06:00 -0700

    Added Kafka 0.10.0 docker image

--- DEVELOPMENT.md
+++ DEVELOPMENT.md
@@ -25,7 +25,7 @@ pub run test -j 1
 
 ## Kafka 0.10.0.0
 
-We're using `ches/kafka` base image so instructions are a bit different:
+We're using `ches/kafka` base image so instructions are a bit different.
 
 ```
 # Zookeeper is in a separate container now
@@ -34,28 +34,10 @@ docker run -d --name zookeeper --publish 2181:2181 jplock/zookeeper:3.4.6
 # Build our image
 docker build -t kafka tool/kafka-0.10.0.0/
 
-# Start first Kafka broker in it's own container
-docker run -d \
-    --hostname 192.168.99.100 \
-    --name kafka1 \
-    --publish 9092:9092 \
-    --env KAFKA_ADVERTISED_HOST_NAME=192.168.99.100 --env ZOOKEEPER_IP=192.168.99.100 \
-    --env KAFKA_BROKER_ID=1 \
-    kafka
+ZK_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' zookeeper)
 
-# Start second Kafka broker in it's own container
-docker run -d \
-    --hostname 192.168.99.100 \
-    --name kafka2 \
-    --publish 9093:9093 \
-    --env KAFKA_ADVERTISED_HOST_NAME=192.168.99.100 --env ZOOKEEPER_IP=192.168.99.100 \
-    --env KAFKA_PORT=9093 --env KAFKA_ADVERTISED_PORT=9093 --env KAFKA_BROKER_ID=2 \
-    kafka
-```
-
-Optionally, can force it to create a topic
-
-```
-docker run --rm kafka \
-  kafka-topics.sh --create --topic bootstrapTopic --replication-factor 2 --partitions 3 --zookeeper 192.168.99.100:2181
+docker run -d --name kafka --publish 9092:9092 --publish 9093:9093 \
+  --env KAFKA_ADVERTISED_HOST_NAME=127.0.0.1 \
+  --env ZOOKEEPER_IP=$ZK_IP \
+  kafka
 ```
--- tool/kafka-0.10.0.0/start.sh
+++ tool/kafka-0.10.0.0/start.sh
@@ -14,12 +14,26 @@ IP=$(grep ${HOSTNAME} /etc/hosts | awk '{print $1}')
 cat /kafka/config/server.properties.template | sed \
   -e "s|{{ZOOKEEPER_CONNECTION_STRING}}|${ZOOKEEPER_CONNECTION_STRING}|g" \
   -e "s|{{ZOOKEEPER_CHROOT}}|${ZOOKEEPER_CHROOT:-}|g" \
-  -e "s|{{KAFKA_BROKER_ID}}|${KAFKA_BROKER_ID:-0}|g" \
+  -e "s|{{KAFKA_BROKER_ID}}|${KAFKA_BROKER_ID:-1}|g" \
   -e "s|{{KAFKA_ADVERTISED_HOST_NAME}}|${KAFKA_ADVERTISED_HOST_NAME:-$IP}|g" \
   -e "s|{{KAFKA_PORT}}|${KAFKA_PORT:-9092}|g" \
   -e "s|{{KAFKA_ADVERTISED_PORT}}|${KAFKA_ADVERTISED_PORT:-9092}|g" \
   -e "s|{{KAFKA_DELETE_TOPIC_ENABLE}}|${KAFKA_DELETE_TOPIC_ENABLE:-false}|g" \
-   > /kafka/config/server.properties
+  -e "s|{{KAFKA_DATA_DIR}}|${KAFKA_DATA_DIR:-/data1}|g" \
+  -e "s|{{KAFKA_DATA_DIRS}}|${KAFKA_DATA_DIRS:-/data1}|g" \
+   > /kafka/config/server1.properties
+
+cat /kafka/config/server.properties.template | sed \
+  -e "s|{{ZOOKEEPER_CONNECTION_STRING}}|${ZOOKEEPER_CONNECTION_STRING}|g" \
+  -e "s|{{ZOOKEEPER_CHROOT}}|${ZOOKEEPER_CHROOT:-}|g" \
+  -e "s|{{KAFKA_BROKER_ID}}|${KAFKA_BROKER_ID:-2}|g" \
+  -e "s|{{KAFKA_ADVERTISED_HOST_NAME}}|${KAFKA_ADVERTISED_HOST_NAME:-$IP}|g" \
+  -e "s|{{KAFKA_PORT}}|${KAFKA_PORT:-9093}|g" \
+  -e "s|{{KAFKA_ADVERTISED_PORT}}|${KAFKA_ADVERTISED_PORT:-9093}|g" \
+  -e "s|{{KAFKA_DELETE_TOPIC_ENABLE}}|${KAFKA_DELETE_TOPIC_ENABLE:-false}|g" \
+  -e "s|{{KAFKA_DATA_DIR}}|${KAFKA_DATA_DIR:-/data2}|g" \
+  -e "s|{{KAFKA_DATA_DIRS}}|${KAFKA_DATA_DIRS:-/data2}|g" \
+  > /kafka/config/server2.properties
 
 # Kafka's built-in start scripts set the first three system properties here, but
 # we add two more to make remote JMX easier/possible to access in a Docker
@@ -34,14 +48,20 @@ cat /kafka/config/server.properties.template | sed \
 # hosts running in a VM with Docker Machine, etc. See:
 #
 # https://issues.apache.org/jira/browse/CASSANDRA-7087
-if [ -z $KAFKA_JMX_OPTS ]; then
-    KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote=true"
-    KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
-    KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.ssl=false"
-    KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT"
-    KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Djava.rmi.server.hostname=${JAVA_RMI_SERVER_HOSTNAME:-$KAFKA_ADVERTISED_HOST_NAME} "
-    export KAFKA_JMX_OPTS
-fi
+# if [ -z $KAFKA_JMX_OPTS ]; then
+#     KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote=true"
+#     KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
+#     KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.ssl=false"
+#     KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT"
+#     KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Djava.rmi.server.hostname=${JAVA_RMI_SERVER_HOSTNAME:-$KAFKA_ADVERTISED_HOST_NAME} "
+#     export KAFKA_JMX_OPTS
+# fi
 
-echo "Starting kafka"
-exec /kafka/bin/kafka-server-start.sh /kafka/config/server.properties
+export JMX_PORT=7203
+echo "Starting kafka1"
+nohup bash -c "/kafka/bin/kafka-server-start.sh /kafka/config/server1.properties 2>&1 &"
+sleep 2
+
+export JMX_PORT=7204
+echo "Starting kafka2"
+exec /kafka/bin/kafka-server-start.sh /kafka/config/server2.properties
