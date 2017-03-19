3714cbd390a3d280b56a6fe836cd96e020619cf0
Anatoly Pulyaevskiy <anatoly@sparkcentral.com>
2017-03-18 21:15:52 -0700

    Attempt to fix travis build

---
+++ tool/rebuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker kill kafka zookeeper
+docker rm kafka zookeeper
+
+docker run -d --name zookeeper --publish 2181:2181 jplock/zookeeper:3.4.6
+
+docker build -t kafka tool/kafka-0.10.1.0/
+
+ZK_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' zookeeper)
+
+docker run -d --name kafka --publish 9092:9092 --publish 9093:9093 \
+  --env KAFKA_ADVERTISED_HOST_NAME=127.0.0.1 \
+  --env ZOOKEEPER_IP=$ZK_IP \
+  kafka
+
+sleep 5
