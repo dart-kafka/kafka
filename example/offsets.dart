af2ca10aafc59e46a0892739fe8e09ceeeda3d19
Larry Aasen <larryaasen@gmail.com>
2020-04-09 15:01:25 -0400

    Removed Dart 1.x new keywords from examples.

--- example/offsets.dart
+++ example/offsets.dart
@@ -3,12 +3,12 @@ import 'dart:async';
 import 'package:kafka/kafka.dart';
 
 Future main() async {
-  var session = new Session(['127.0.0.1:9092']);
-  var master = new OffsetMaster(session);
+  var session = Session(['127.0.0.1:9092']);
+  var master = OffsetMaster(session);
   var offsets = await master.fetchEarliest([
-    new TopicPartition('simple_topic', 0),
-    new TopicPartition('simple_topic', 1),
-    new TopicPartition('simple_topic', 2),
+    TopicPartition('simple_topic', 0),
+    TopicPartition('simple_topic', 1),
+    TopicPartition('simple_topic', 2),
   ]);
   print(offsets);
   await session.close(); // Always close session in the end.
--- example/simple_producer.dart
+++ example/simple_producer.dart
@@ -5,15 +5,15 @@ Future main() async {
   // Logger.root.level = Level.ALL;
   // Logger.root.onRecord.listen(print);
 
-  var config = new ProducerConfig(bootstrapServers: ['127.0.0.1:9092']);
-  var producer = new Producer<String, String>(
-      new StringSerializer(), new StringSerializer(), config);
+  var config = ProducerConfig(bootstrapServers: ['127.0.0.1:9092']);
+  var producer =
+      Producer<String, String>(StringSerializer(), StringSerializer(), config);
 
   for (var i = 0; i < 10; i++) {
     // Loop through a list of partitions.
     for (var p in [0, 1, 2]) {
       var rec =
-          new ProducerRecord('simple_topic', p, 'key:${p},$i', 'value:${p},$i');
+          ProducerRecord('simple_topic', p, 'key:${p},$i', 'value:${p},$i');
       producer.add(rec);
       rec.result.then(print);
     }
